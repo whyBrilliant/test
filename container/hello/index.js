6f775e500476eb8730aeb816a71303108408e66b
whyBrilliant <291917996@qq.com>
2018-04-09 07:47:58 +0800

    第一次提交

---
+++ container/hello/index.js
@@ -0,0 +1,18 @@
+import  React, { Component } from "react";
+import { Input } from 'antd'
+// import { Input } from 
+
+class Hello extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (<div>
+            {/* Hello World */}
+            <Input />
+        </div>)
+    }
+}
+
+export default Hello
