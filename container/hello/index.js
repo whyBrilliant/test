915384334beee9a0e424ac7ebdc899deecb61e22
whyBrilliant <291917996@qq.com>
2018-04-09 23:59:54 +0800

    add antd component

--- container/hello/index.js
+++ container/hello/index.js
@@ -1,18 +1,100 @@
-import  React, { Component } from "react";
-import { Input } from 'antd'
+import React, { Component } from "react";
+import { Input, Form, Select, DatePicker, Row, Col, Button } from 'antd'
 // import { Input } from 
+const FormItem = Form.Item
+const Option = Select.Option
+import 'antd/dist/antd.css'; 
 
 class Hello extends Component {
     constructor(props) {
         super(props)
     }
 
-    render() {
-        return (<div>
-            {/* Hello World */}
-            <Input />
-        </div>)
+    handleSearch() {
+        this.props.form.validateFields((err, value) => {
+            if (!err) {
+                console.log(err)
+            }
+            console.log(value)
+        })
     }
-}
 
-export default Hello
+    render() {
+        const { getFieldDecorator } = this.props.form
+        return (<div>
+            <Form>
+                <Row>
+                    <Col span="12">
+                        <FormItem
+                            label="Name"
+                            labelCol={{ span: 5 }}
+                            wrapperCol={{ span: 12 }}
+                        >
+                            {getFieldDecorator('name', {
+                                rules: [{ required: true, message: 'Please input your name!' }],
+                            })(
+                                <Input />
+                            )}
+                        </FormItem>
+                    </Col>
+                    <Col span="12">
+                        <FormItem
+                            label="Gender"
+                            labelCol={{ span: 5 }}
+                            wrapperCol={{ span: 12 }}
+                        >
+                            {getFieldDecorator('gender', {
+                                rules: [{ required: true, message: 'Please select your gender!' }],
+                            })(
+                                <Select>
+                                    <Option value="male">male</Option>
+                                    <Option value="female">female</Option>
+                                </Select>
+                            )}
+                        </FormItem>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col span="12">
+                        <FormItem
+                            label="Age"
+                            labelCol={{ span: 5 }}
+                            wrapperCol={{ span: 12 }}
+                        >
+                            {getFieldDecorator('age', {
+                                rules: [{ required: true, message: 'Please input your age!' }],
+                            })(
+                                <Input />
+                            )}
+                        </FormItem>
+                    </Col>
+                    <Col span="12">
+                        <FormItem
+                            label="Color"
+                            labelCol={{ span: 5 }}
+                            wrapperCol={{ span: 12 }}
+                        >
+                            {getFieldDecorator('color', {
+                                rules: [{ required: true, message: 'Please select your color!' }],
+                            })(
+                                <Select
+                                    placeholder="请输入颜色"
+                                >
+                                    <Option value="red">red</Option>
+                                    <Option value="yellow">yellow</Option>
+                                </Select>
+                            )}
+                        </FormItem>
+                    </Col>
+                </Row>
+                <Row type="flex">
+                    <Col span="4"><Button onClick={this.handleSearch.bind(this)}>确定</Button></Col>
+                </Row>
+            </Form>
+        </div>)
+            }
+        }
+
+        Hello = Form.create()(Hello)
+        
+        export default Hello
